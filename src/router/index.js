7911b6652ccf453b2bd75898550f8bf2a0fbb56a
LiZheGuang <1030660726@qq.com>
2018-12-16 14:09:06 +0800

    fix(js) : debug

--- src/router/index.js
+++ src/router/index.js
@@ -5,21 +5,12 @@ Vue.use(Router)
 
 const login = r => require.ensure([], () => r(require('@/page/login')), 'login');
 const manage = r => require.ensure([], () => r(require('@/page/manage')), 'manage');
-const home = r => require.ensure([], () => r(require('@/page/home')), 'home');
-const addShop = r => require.ensure([], () => r(require('@/page/addShop')), 'addShop');
 const addGoods = r => require.ensure([], () => r(require('@/page/addGoods')), 'addGoods');
 const userList = r => require.ensure([], () => r(require('@/page/userList')), 'userList');
-const shopList = r => require.ensure([], () => r(require('@/page/shopList')), 'shopList');
 const foodList = r => require.ensure([], () => r(require('@/page/foodList')), 'foodList');
 const orderList = r => require.ensure([], () => r(require('@/page/orderList')), 'orderList');
 const adminList = r => require.ensure([], () => r(require('@/page/adminList')), 'adminList');
-// const visitor = r => require.ensure([], () => r(require('@/page/visitor')), 'visitor');
-// const newMember = r => require.ensure([], () => r(require('@/page/newMember')), 'newMember');
-// const uploadImg = r => require.ensure([], () => r(require('@/page/uploadImg')), 'uploadImg');
-// const vueEdit = r => require.ensure([], () => r(require('@/page/vueEdit')), 'vueEdit');
-// const adminSet = r => require.ensure([], () => r(require('@/page/adminSet')), 'adminSet');
-// const sendMessage = r => require.ensure([], () => r(require('@/page/sendMessage')), 'sendMessage');
-// const explain = r => require.ensure([], () => r(require('@/page/explain')), 'explain');
+
 
 const routes = [
 	{
@@ -31,14 +22,6 @@ const routes = [
 		component: manage,
 		name: '',
 		children: [{
-			path: '',
-			component: home,
-			meta: [],
-		},{
-			path: '/addShop',
-			component: addShop,
-			meta: ['添加数据', '添加商铺'],
-		},{
 			path: '/addGoods',
 			component: addGoods,
 			meta: ['添加数据', '添加商品'],
@@ -48,10 +31,6 @@ const routes = [
 			meta: ['数据管理', '用户列表'],
 		}
 		,{
-			path: '/shopList',
-			component: shopList,
-			meta: ['数据管理', '商家列表'],
-		},{
 			path: '/foodList',
 			component: foodList,
 			meta: ['数据管理', '商品列表'],
